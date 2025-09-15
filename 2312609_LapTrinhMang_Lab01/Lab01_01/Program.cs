3ecc17ce8552c3220d2cc21ac836462bc5ede955
TaiKhoanGitHub411 <danhchogithub@gmail.com>
2025-09-15 14:15:06 +0700

    Lab 01 - Xong bài 1,2

---
+++ 2312609_LapTrinhMang_Lab01/Lab01_01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01_01
+{
+	class Program
+	{
+		static void GetHostInfo(string host)
+		{
+			try
+			{
+				IPHostEntry hostInfo = Dns.GetHostEntry(host);
+				Console.WriteLine("Tên miền: " + hostInfo.HostName);
+				Console.Write("Địa chỉ IP: ");
+				foreach (IPAddress ipadrr in hostInfo.AddressList)
+				{
+					Console.WriteLine(ipadrr.ToString() + " ");
+				}
+				Console.WriteLine();
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("Không phân giải đươc tên miền: " + host + "\n");
+			}
+		}
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+			foreach (String arg in args)
+			{
+				Console.WriteLine("Phân giải tên miền: " + arg);
+				GetHostInfo(arg);
+			}
+			Console.ReadKey();
+		}
+	}
+}
---
+++ 2312609_LapTrinhMang_Lab01/Lab01_02/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Lab01_02
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+			ThongTinIP();
+			Console.ReadKey();
+		}
+		static void ThongTinIP()
+		{
+			try
+			{
+				//Lấy các Network interface
+				NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+				foreach(var nic in nics)
+				{
+					//Kiểm tra card mạng có hoạt động
+					if(nic.OperationalStatus==OperationalStatus.Up)
+					{
+						//lấy thông tin IP trong mạng này
+						IPInterfaceProperties pro = nic.GetIPProperties();
+						//Lấy IPV4
+						foreach(UnicastIPAddressInformation ip in pro.UnicastAddresses)
+						{
+							if(ip.Address.AddressFamily==System.Net.Sockets.AddressFamily.InterNetwork)
+							{
+								Console.WriteLine("Địa chỉ IP: " + ip.Address.ToString());
+								Console.WriteLine("Subnet mask: " + ip.IPv4Mask.ToString());
+							}	
+						}
+						//lấy default gateway
+						foreach(GatewayIPAddressInformation gateway in pro.GatewayAddresses)
+						{
+							Console.WriteLine("Default Gateway: " + gateway.Address.ToString());
+						}
+						Console.WriteLine();
+					}	
+				}	
+
+			}
+			catch(Exception ex)
+			{
+				Console.WriteLine("Có lỗi xảy ra: " + ex.Message);
+			}
+		}
+	}
+}
