86b691b3f86996911b2b2cd4db3452462914352c
TaiKhoanGitHub411 <danhchogithub@gmail.com>
2025-09-20 20:57:31 +0700

    Xong Lab02 HD3

---
+++ 2312609_LapTrinhMang_Lab02/Lab02_HD2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Lab02_HD2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, 5000);            
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);            serverSocket.Bind(serverEndPoint);
+            serverSocket.Listen(10);
+            Console.WriteLine("Đang chờ kết nối từ client ...");
+            Socket clientSocket = serverSocket.Accept();            
+            EndPoint clientEndPoint = clientSocket.RemoteEndPoint;
+            Console.WriteLine("Chấp nhận kết nối từ: " + clientEndPoint.ToString());
+            string hello = "Hello client";
+            //Mảng chứa dữ liệu
+            byte[] buff;
+            //Chuyển chuỗi thành mảng byte
+            buff = Encoding.ASCII.GetBytes(hello);
+            //Chuyển nội dung câu chào đến client
+            clientSocket.Send(buff, 0, buff.Length, SocketFlags.None);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2312609_LapTrinhMang_Lab02/Lab02_HD3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Lab02_HD3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Loopback, 5000);
+            // Tạo Socket cho client
+            // Cấu hình socket phải giống với server (TCP/IP).
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, 
+                SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("Đang kết nối với Server ...");
+            // Kết nối đến server
+            serverSocket.Connect(serverEndPoint);
+            // Kiểm tra kết nối và nhận dữ liệu
+            if (serverSocket.Connected)
+            {
+                Console.WriteLine("Kết nối thành công với server...");              
+                byte[] buff = new byte[1024];
+                // Nhận dữ liệu từ server và lưu vào buffer.
+                // Phương thức Receive trả về số byte thực sự nhận được.
+                int byteReceive = serverSocket.Receive(buff, 0, buff.Length, SocketFlags.None);
+                // Chuyển đổi mảng byte nhận được thành chuỗi.
+                // Chỉ chuyển đổi số byte thực sự nhận được (byteReceive).
+                string str = Encoding.ASCII.GetString(buff, 0, byteReceive);
+                Console.WriteLine("Server trả lời " + str);
+            }
+            Console.ReadLine();
+        }
+    }
+}
