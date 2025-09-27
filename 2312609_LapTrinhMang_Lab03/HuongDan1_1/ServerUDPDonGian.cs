1011b5c1fb3f3b844bcebe8d5383b0b72842a00b
TaiKhoanGitHub411 <danhchogithub@gmail.com>
2025-09-27 08:42:12 +0700

    Cải tiến HD1 theo yêu cầu bài tập - có tình trạng mất gói tin với chương trình này

---
+++ 2312609_LapTrinhMang_Lab03/HuongDan1_1/ServerUDPDonGian.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace HuongDan1_1
+{
+    class ServerUDPDonGian
+    {
+        static void Main(string[] args)
+        {   
+            //SERVER UDP ĐƠN GIẢN
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);//Điểm cuối mạng
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);//Điểm giao tiếp với client
+            serverSocket.Bind(serverEndPoint);//Liên kết server với end point
+            Console.WriteLine("Chờ kết nối ...");
+            //Phần cải tiến theo yêu cầu bài tập 
+            //Chương trình này đang gấy mất thông điệp
+            while(true)//Vòng lặp nhận tin nhắn từ client liên tục
+            {
+                byte[] buff = new byte[1024];//chứa dữ liệu nhận được
+                EndPoint remote = new IPEndPoint(IPAddress.Any, 0);//remote lưu thông tin client
+                int bytesReceived = serverSocket.ReceiveFrom(buff, 0, buff.Length, SocketFlags.None, ref remote);
+                //Hiển thị thông điệp từ client
+                string msg = Encoding.UTF8.GetString(buff, 0, bytesReceived);
+                Console.WriteLine($"Nội dung tin nhắn từ client {remote}: {msg}");
+                //Đóng server nếu tin nhắn là 'exit all'
+                if(msg.ToLower()=="exit all")
+                {
+                    Console.WriteLine($"Đóng server ...");
+                    break;
+                }
+            }
+            serverSocket.Close();
+            Console.WriteLine("Đã đóng server. Nhấn phím bất kỳ để thoát.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2312609_LapTrinhMang_Lab03/HuongDan1_2/ClientUDPDonGian.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace HuongDan1_2
+{
+    class ClientUDPDonGian
+    {
+        static void Main(string[] args)
+        {
+            //CLIENT UDP ĐƠN GIẢN
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);// đối tượng gửi dữ liệu kiểu UDP.
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);//Địa chỉ, port cửa server nhận dữ liệu
+            //Phần cải tiến theo yêu cầu bài tập
+            while(true)
+            {
+                Console.WriteLine("Nhập tin nhắn gửi server (nhập exit để thoát, nhập exit all để thoát client và server): ");
+                string msg = Console.ReadLine();
+                //Chuỗi lưu thông điệp
+                byte[] buff = Encoding.UTF8.GetBytes(msg);
+                //Gửi dữ liệu
+                clientSocket.SendTo(buff, buff.Length, SocketFlags.None, serverEndPoint);
+                //Kiểm tra điều kiện thoát
+                if(msg.ToLower()=="exit")
+                {
+                    Console.WriteLine("Đóng client ...");
+                    break;
+                }
+                if(msg.ToLower()=="exit all")
+                {
+                    Console.WriteLine("Đang đóng server. Client tự động đóng");
+                    break;
+                }
+            }
+            clientSocket.Close();
+            Console.WriteLine("Đã đóng client. Nhấn phím bất kỳ để thoát");
+            Console.ReadKey();
+        }
+    }
+}
